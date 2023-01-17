eed10449d71840d13952e27279ed9a896bb63088
LucasFlaquer <lucas.flaquer@gmail.com>
2023-01-17 18:50:18 -0300

    setup stitches

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,13 @@
-import { colors } from '@gaia/tokens'
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite300',
+  borderRadius: '$md',
+  padding: '$2 $4',
+  height: '$12',
+})
 
 export function App() {
-  return <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
+  return <Button>click</Button>
 }
