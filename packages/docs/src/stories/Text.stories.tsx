49a83f2a0f5aee6cc4c123839e1e8e4954092b20
LucasFlaquer <lucas.flaquer@gmail.com>
2023-01-24 13:53:58 -0300

    text component

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@gaia/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente iste voluptas voluptatum consequatur, nam quo illum a? Excepturi ad reprehenderit labore quia, nesciunt accusantium commodi eaque ratione inventore eum dolor quaerat iste est consequatur magni distinctio deleniti possimus hic dolore molestias? Laborum, optio ducimus. Iusto sequi et doloremque corrupti ut.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustoTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
