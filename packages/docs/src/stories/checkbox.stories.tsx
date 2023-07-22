a382991f3e205f116041ba05abf449f0ea54e271
LucasFlaquer <lucas.flaquer@gmail.com>
2023-07-22 16:34:18 -0300

    checkbox component

---
+++ packages/docs/src/stories/checkbox.stories.tsx
@@ -0,0 +1,30 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, CheckboxProps, Checkbox } from '@gaia/react'
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    (Story) => (
+      <Box css={{ display: 'flex', gap: '$2' }}>
+        {Story()}
+        <Text as="label" size="sm">
+          Email Address
+        </Text>
+      </Box>
+    ),
+  ],
+} as Meta<CheckboxProps>
+
+export const Primary: StoryObj<CheckboxProps> = {
+  args: {},
+}
+export const WithPrefix: StoryObj<CheckboxProps> = {
+  args: {},
+}
+export const Disabled: StoryObj<CheckboxProps> = {
+  args: {
+    disabled: true,
+  },
+}
---
+++ packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,16 @@
+import { Check } from 'phosphor-react'
+import { CheckboxContaienr, CheckboxIndicator } from './styles'
+import { ComponentProps } from 'react'
+
+export interface CheckboxProps
+  extends ComponentProps<typeof CheckboxContaienr> {}
+
+export function Checkbox() {
+  return (
+    <CheckboxContaienr>
+      <CheckboxIndicator asChild>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContaienr>
+  )
+}
